380ffa58ef862866a880678db014f834b00e7331
Yakov5776 <28637006+Yakov5776@users.noreply.github.com>
2023-07-23 11:33:42 -0400

    add isJailbroken and ssh proxying capability

--- deviceController.js
+++ deviceController.js
@@ -1,5 +1,7 @@
-const { exec } = require('child_process');
+const { exec, execSync, spawn } = require('child_process');
 var deviceList = [];
+var iproxyList = [];
+let ClientSshPort = 2222;
 
 const getConnectedDevices = async () => {
     return new Promise((resolve, reject) => {
@@ -13,7 +15,7 @@ const getConnectedDevices = async () => {
         } else {
           let devices = stdout.trim().split('\n');
           if (devices.length === 1 && devices[0] === '') devices = [];
-          else devices = devices.map(id => ({ id, status: 'ready' }));
+          else devices = devices.map(id => ({ id, status: 'ready', isJailbroken: isJailbroken(id), ssh: getSsh(id)}));
           resolve(devices);
         }
       });
@@ -26,6 +28,27 @@ const reloadDevices = async function() {
 
 const getDevices = function() {
     return deviceList;
+}
+
+const isJailbroken = function(id) {
+  try {
+    const output = execSync(`ideviceinstaller -u ${id} -l -o list_system`);
+    return output.includes('Cydia');
+  } catch (error) {
+    console.error('Error executing occurred: ', error);
+    return false;
+  }
+};
+
+const getSsh = function(id) {
+  if (!iproxyList[id]) setupSsh(id)
+  return iproxyList[id];
+}
+
+const setupSsh = function(id) {
+  const port = ClientSshPort++;
+  const iproxy = spawn('iproxy', [port, 22, id]);
+  iproxyList[id] = {iproxy_pid: iproxy.pid, port: port};
 }
 
 module.exports = { getDevices, reloadDevices };
